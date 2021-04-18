d352909e8000da22b8c5f465cdc55593655a311f
Milton839 <miltonbiswas839@gmail.com>
2021-04-18 16:37:48 +0600

    css animation done

--- src/Components/Home/ServiceDetails/ServiceDetails.js
+++ src/Components/Home/ServiceDetails/ServiceDetails.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './ServiceDetails.css';
 import {Link} from 'react-router-dom';
 
 const ServiceDetail = ({service}) => {
     return (
-        <div className="col-md-3 col-lg-3 col-xl-3 col-sm-12 col-xs-12 col-12 text-center">
+        <div className="col-md-3 col-lg-3 col-xl-3 col-sm-12 col-xs-12 col-12 text-center service-item">
             <img style={{ height:'50px' }} src={service.image_url} alt=""/>
             <h6 className="my-4 fs-5">${service.price}</h6>
             <h5 className="my-4 fs-5">{service.title}</h5>
