c6c30d8822c3334718141a6d7e28c41aa272c1fc
Milton839 <miltonbiswas839@gmail.com>
2021-04-18 19:58:27 +0600

    sidebar

--- src/Components/Admin/Sidebar/Sidebar.js
+++ src/Components/Admin/Sidebar/Sidebar.js
@@ -21,7 +21,7 @@ const Sidebar = () => {
             .then(data => setIsAdmin(data));
     }, []);
     return (
-        <div className="sidebar d-flex flex-column justify-content-between col-md-6 py-5 px-4" style={{ height: "100vh" }}>
+        <div className="sidebar d-flex flex-column justify-content-between  py-5" style={{ height: "100vh" }}>
             <ul className="list-unstyled">
                     {isAdmin && <div>
                         <li>
